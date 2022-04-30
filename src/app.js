f65ee212cd0e546a19cb62fbc63284a1e92abd3a
douglas ontiveros <douglasontiveros1408@gmail.com>
2022-04-30 17:51:28 -0400

    config en cors

--- src/app.js
+++ src/app.js
@@ -10,6 +10,13 @@ app.set("port", process.env.PORT || 3001)
 app.use(cors())
 app.use(express.json())
 
+const corsOptions ={
+    origin:'https://crud-mern-ontiveros.herokuapp.com', 
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 //ROUTERS
 app.use("/api/users", require("./routers/users"))
 app.use("/api/notes", require("./routers/notes"))
