37f5e3237387be1c7c9cbe836cd209a324b1f1aa
douglas ontiveros <douglasontiveros1408@gmail.com>
2022-04-30 21:12:40 -0400

    deduggin cors agrege origin6

--- src/app.js
+++ src/app.js
@@ -8,16 +8,9 @@ app.set("port", process.env.PORT || 3001)
 //MIDLEWARE
 
 
-const whiteList = ["https://ontiveros-js.github.io/crud-mern-ontiveros"]
+const whiteList = ["https://ontiveros-js.github.io/crud-mern-ontiveros/", "ontiveros-js.github.io/crud-mern-ontiveros"]
 app.use(cors({origin: whiteList}))
 app.use(express.json())
-
-// const corsOptions ={
-//     origin:'https://crud-mern-ontiveros.herokuapp.com:35510', 
-//     credentials:true,
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
 
 //ROUTERS
 app.use("/api/notes", require("./routers/notes"))
